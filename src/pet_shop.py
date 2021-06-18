336bd0b4635290e4ac4ecc650005788b99b0ac26
Connor <connorjrobertson@hotmail.co.uk>
2021-06-18 12:53:04 +0100

    Functions 3 and 4 [add or remove cash] fucntioning correctly

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -11,3 +11,5 @@ def get_total_cash(pet_shop):
 def add_or_remove_cash(pet_shop, cash_amount):
     #need to update the dictionary with the change in cash value
         #in the function that calls this one it then simply calls the new cash value using the previously established get_total_cash()
+    pet_shop["admin"]["total_cash"] = (pet_shop["admin"]["total_cash"] + cash_amount)
+
