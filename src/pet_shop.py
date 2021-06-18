4c2e03caf5377880486a04d487636965f43c707f
Connor <connorjrobertson@hotmail.co.uk>
2021-06-18 12:45:17 +0100

    Second function 'Get_total_cash' is working

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,4 +1,13 @@
 # WRITE YOUR FUNCTIONS HERE
 
+import pdb
+
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, cash_amount):
+    #need to update the dictionary with the change in cash value
+        #in the function that calls this one it then simply calls the new cash value using the previously established get_total_cash()
