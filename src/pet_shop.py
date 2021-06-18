ed2fd5899b4667c9b28851dd9e622c7c8f1a2bd6
Connor <connorjrobertson@hotmail.co.uk>
2021-06-18 15:39:11 +0100

    get_customer_pet_count and add_pet_to_customer functions testing correctly

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -63,4 +63,9 @@ def get_customer_cash(customer):
 def remove_customer_cash(customer, cash_payment):
     customer["cash"] -= cash_payment
 
+#function returns the length of the pets list within the specified customers dictionary, i.e how many pets they have
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
 
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
